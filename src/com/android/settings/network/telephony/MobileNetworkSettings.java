f99167b24d884c6c039b7dca5873e669fd3496b9
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-06-16 20:18:22 +0530

    Settings: Add toggle for Smart 5G [2/2]
    Change-Id: I72958e2313b48553ba82565ace8ebf5dd9e0cdf4
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- src/com/android/settings/network/telephony/MobileNetworkSettings.java
+++ src/com/android/settings/network/telephony/MobileNetworkSettings.java
@@ -322,6 +322,7 @@ public class MobileNetworkSettings extends AbstractMobileNetworkSettings impleme
                 subscriptionPreferenceControllers) {
             controller.init(mSubId);
         }
+        use(Smart5gPreferenceController.class).init(mSubId);
     }
 
     @Override
